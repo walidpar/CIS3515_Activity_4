ee9c511e8c492bd6ecf60c4ce066731f46d57a24
Walid Parast <walidparast1@gmail.com>
2025-02-04 12:05:21 -0500

    Working on spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -1,26 +1,25 @@
 package edu.temple.inclassuiacvitivity
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        val displayTextView = findViewById<TextView>(R.id.textDisplay)
+        spinner.adapter = adapter
 
-        /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        //val numberArray = Array Of Numbers
+        val oISL = object: OnItemClickListener {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
-        /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+            }
 
+            override fun onNothingSelected(p0: AdapterView<*>?) {
 
-        // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
-
+            }
+        }
     }
 }
